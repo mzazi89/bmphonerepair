4042a746429284b68e0efc24b42941209203193c
mzazi89 <mzazitechinc@gmail.com>
2026-07-29 17:02:12 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import ServicesSection from './components/ServicesSection'
+import Testimonials from './components/Testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ServicesSection />
+      <Testimonials />
     </>
   )
 }
